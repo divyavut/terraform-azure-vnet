2327d21ea7eafc12e47d3cd2d6c23f7aabb33965
divyavut <vdivyaaws@gmail.com>
2024-11-23 15:59:51 -0500

    azure vnet

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   resource_name = "${var.project_name}-${var.environment}"
-  rg_name = data.azurerm_app_configuration_key.rg_name.value
-  rg_location = data.azurerm_app_configuration_key.rg_location.value
+  rg_location = data.azurerm_resource_group.main.location
+  rg_name= data.azurerm_resource_group.main.name
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "vnet_id" {
-  value       = azurerm_virtual_network.main.id
+value       = azurerm_virtual_network.main.id
 }
 output "public_subnet_id" {
   value = azurerm_subnet.public.id
